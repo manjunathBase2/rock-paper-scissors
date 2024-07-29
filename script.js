5967446f5951517f380e70cf7875917ad4c8ce44
Manjunath P <manjunathworkspace@gmail.com>
2024-07-30 00:05:50 +0530

    chore: Refactor game logic and improve user experience

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
+let humanScore = 0,
+  computerScore = 0;
 
-let humanScore = 0
-,computerScore = 0;
-
-function getComputerChoice(){
-    let randInt = Math.floor(Math.random()*3);
-    if (randInt==0) return 'rock';
-    if (randInt==1) return 'paper';
-    if (randInt==2) return 'scissor';
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissor"];
+  const randInt = Math.floor(Math.random() * 3);
+  return choices[randInt];
 }
 
-function getHumanChoice(){
-    let input = prompt("Choose an option:",'Rock');
-    input = input.toLowerCase();
-    console.log(input);
+function getHumanChoice() {
+  let input = prompt(
+    "Choose an option (rock, paper, scissor):",
+    ""
+  ).toLowerCase();
+  while (input !== "rock" && input !== "paper" && input !== "scissor") {
+    console.log("Invalid input! Please try again.");
+    input = prompt(
+      "Choose an option (rock, paper, scissor):",
+      ""
+    ).toLowerCase();
+  }
+  return input;
 }
 
-const humanSelection = getHumanChoice();
-const compterSelection = getComputerChoice();
+function playRound(humanSelection, computerSelection) {
+  console.log(
+    `Your choice: ${humanSelection} || Computer Choice: ${computerSelection}`
+  );
 
-function playRound(humanSelection,computerChoice){
-    
-
+  if (humanSelection === computerSelection) return "It is a tie!";
+  if (
+    (humanSelection === "rock" && computerSelection === "scissor") ||
+    (humanSelection === "scissor" && computerSelection === "paper") ||
+    (humanSelection === "paper" && computerSelection === "rock")
+  ) {
+    humanScore++;
+    return `You win! ${humanSelection} beats ${computerSelection}`;
+  } else {
+    computerScore++;
+    return `You lose! ${computerSelection} beats ${humanSelection}`;
+  }
 }
+
+function playGame() {
+  let n = prompt("Enter the number of rounds you want to play: ", "");
+  for (let i = 0; i < n; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(humanSelection, computerSelection));
+  }
+  console.log(
+    `Final Scores - Human: ${humanScore} || Computer: ${computerScore}`
+  );
+}
+
+playGame();
